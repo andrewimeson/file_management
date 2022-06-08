0b46ca9ced2b260ebdf6cbf5847646b168a214e2
Andrew Imeson <andrew@andrewimeson.com>
2022-06-07 21:13:16 -0400

    Add /hello-world API route

--- file_management/app.py
+++ file_management/app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
 
 @app.route("/")
 def index():
     return "Hello World"
 
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"hello": "world"}
+
+
+api.add_resource(HelloWorld, "/hello-world")
 
 if __name__ == "__main__":
     app.run()
